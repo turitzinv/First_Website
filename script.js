8c5911077befcd85f6ba3c519c9481f7ff20946b
Victor Turitzin <turitzinv@gmail.com>
2021-11-16 18:58:48 -0500

    Added a fun button click to test JavaScript

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2); */
 
-const address = {
+/*const address = {
     street: "44 Longstreet Road",
     city: "Holmdel",
     state: "NJ",
     zipCode: "07733",
-};
+}; */
 
-const location = document.getElementById('location');
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+    button.textContent = `Click count: ${event.detail}`;
+})
+
+/*const location = document.getElementById('location');
 
 function clickAlert() {
     alert ('I was clicked!');
 }
 
-location.addEventListener('click', clickAlert);
+location.addEventListener('click', clickAlert); */
