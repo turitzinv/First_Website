8c3f9ea4edbcd283c43b571d227d028773a4786d
Victor Turitzin <turitzinv@gmail.com>
2021-11-15 19:22:11 -0500

    Centered first paragraph and added border color.  Adding a Location button on top but still needs to be fixed to show address upon clicking

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2); */
+
+const address = {
+    street: "44 Longstreet Road",
+    city: "Holmdel",
+    state: "NJ",
+    zipCode: "07733",
+};
+
+const location = document.getElementById('location');
+
+function clickAlert() {
+    alert ('I was clicked!');
+}
+
+location.addEventListener('click', clickAlert);
